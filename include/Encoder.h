5986d06b3d9639569e97042c095246dc94049584
Arsenii Iarmolinskii <yarmolinskiyam@gmail.com>
2025-10-17 18:21:24 +0300

    encoders

--- include/Encoder.h
+++ include/Encoder.h
@@ -5,15 +5,22 @@
 #define ENC_LEFT_CLK_A_PIN 2
 #define ENC_LEFT_B_PIN 4
 
-#define ENC_LEFT_DIR 1 // {-1, 1}
+#define ENC_RIGHT_CLK_A_PIN 3
+#define ENC_RIGHT_B_PIN 5
+
+#define ENC_LEFT_DIR 1   // {-1, 1}
+#define ENC_RIGHT_DIR -1 // {-1, 1}
 
 #define ENC_TICK_TO_RAD (2 * M_PI / (48 * 30))
 
 int8_t enc_l_tt[4][4] = {0}; // Transition table
+int8_t enc_r_tt[4][4] = {0};
 
 float enc_l_phi = 0;
+float enc_r_phi = 0;
 
 volatile int enc_l_counter = 0;
+volatile int enc_r_counter = 0;
 
 void enc_l_interrupt()
 {
@@ -43,6 +50,20 @@ void enc_l_interrupt()
     old_AB = AB;
 }
 
+void enc_r_interrupt()
+{
+    int B = digitalRead(ENC_RIGHT_B_PIN);
+    int CA = digitalRead(ENC_RIGHT_CLK_A_PIN);
+    int A = CA ^ B;
+    int AB = A << 1 | B;
+
+    static int old_AB = AB;
+
+    enc_r_counter += enc_r_tt[old_AB][AB];
+
+    old_AB = AB;
+}
+
 void enc_l_init()
 {
     pinMode(ENC_LEFT_CLK_A_PIN, INPUT);
@@ -61,14 +82,42 @@ void enc_l_init()
     attachInterrupt(
         digitalPinToInterrupt(ENC_LEFT_CLK_A_PIN),
         enc_l_interrupt,
-        CHANGE
-    );
+        CHANGE);
+}
+
+void enc_r_init()
+{
+    pinMode(ENC_RIGHT_CLK_A_PIN, INPUT);
+    pinMode(ENC_RIGHT_B_PIN, INPUT);
+
+    enc_r_tt[0b00][0b01] = ENC_RIGHT_DIR;
+    enc_r_tt[0b01][0b11] = ENC_RIGHT_DIR;
+    enc_r_tt[0b11][0b10] = ENC_RIGHT_DIR;
+    enc_r_tt[0b10][0b00] = ENC_RIGHT_DIR;
+
+    enc_r_tt[0b00][0b10] = -ENC_RIGHT_DIR;
+    enc_r_tt[0b10][0b11] = -ENC_RIGHT_DIR;
+    enc_r_tt[0b11][0b01] = -ENC_RIGHT_DIR;
+    enc_r_tt[0b01][0b00] = -ENC_RIGHT_DIR;
+
+    attachInterrupt(
+        digitalPinToInterrupt(ENC_RIGHT_CLK_A_PIN),
+        enc_r_interrupt,
+        CHANGE);
 }
 
 int enc_l_get_ticks()
 {
     noInterrupts();
     int ret = enc_l_counter;
+    interrupts();
+    return ret;
+}
+
+int enc_r_get_ticks()
+{
+    noInterrupts();
+    int ret = enc_r_counter;
     interrupts();
     return ret;
 }
@@ -83,7 +132,22 @@ void enc_l_tick()
     enc_l_phi += ENC_TICK_TO_RAD * counter;
 }
 
+void enc_r_tick()
+{
+    noInterrupts();
+    int counter = enc_r_counter;
+    enc_r_counter = 0;
+    interrupts();
+
+    enc_r_phi += ENC_TICK_TO_RAD * counter;
+}
+
 float enc_l_get_phi()
 {
     return enc_l_phi;
+}
+
+float enc_r_get_phi()
+{
+    return enc_r_phi;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,54 +9,53 @@ int left_u = 0;
 int right_u = 0;
 
 SCREEN(volts,
-  {
-    ROW("Vbatt [mV]: %d", int(vs_get_v_batt() * 1000));
-    CLICK_ROW(
-      [](CLICK_STATE state){
-        switch (state)
-        {
-        case CLICK_LEFT:
-          left_u--;
-          break;
-        case CLICK_RIGHT:
-          left_u++;
-          break;
-        case CLICK_DOWN:
-          left_u = 0;
-          break;
-        default:
-          break;
-        }
-      },
-      "left u [V]: %d", left_u
-    )
-    CLICK_ROW(
-      [](CLICK_STATE state){
-        switch (state)
-        {
-        case CLICK_LEFT:
-          right_u--;
-          break;
-        case CLICK_RIGHT:
-          right_u++;
-          break;
-        case CLICK_DOWN:
-          right_u = 0;
-          break;
-        default:
-          break;
-        }
-      },
-      "right u [V]: %d", right_u
-    )
-  }
-)
+       {
+         ROW("Vbatt [mV]: %d", int(vs_get_v_batt() * 1000));
+         CLICK_ROW(
+             [](CLICK_STATE state)
+             {
+               switch (state)
+               {
+               case CLICK_LEFT:
+                 left_u--;
+                 break;
+               case CLICK_RIGHT:
+                 left_u++;
+                 break;
+               case CLICK_DOWN:
+                 left_u = 0;
+                 break;
+               default:
+                 break;
+               }
+             },
+             "left u [V]: %d", left_u)
+         CLICK_ROW(
+             [](CLICK_STATE state)
+             {
+               switch (state)
+               {
+               case CLICK_LEFT:
+                 right_u--;
+                 break;
+               case CLICK_RIGHT:
+                 right_u++;
+                 break;
+               case CLICK_DOWN:
+                 right_u = 0;
+                 break;
+               default:
+                 break;
+               }
+             },
+             "right u [V]: %d", right_u)
+       })
 
 SCREEN(encoders,
-  {
-    ROW("Left phi[deg]: %d", (int)(enc_l_get_phi() * 180 / M_PI));
-  }
-)
+       {
+         ROW("Left phi[deg]: %d", (int)(enc_l_get_phi() * 180 / M_PI));
+         ROW("Right phi[deg]: %d", (int)(enc_r_get_phi() * 180 / M_PI));
+       })
 
 void setup()
 {
@@ -65,6 +64,7 @@ void setup()
   m_init();
   vs_init();
   enc_l_init();
+  enc_r_init();
 
   interrupts();
 
@@ -77,6 +77,7 @@ void setup()
 void loop()
 {
   enc_l_tick();
+  enc_r_tick();
 
   m_drive(left_u, right_u);
 
